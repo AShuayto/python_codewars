480c5c1c707fcf0e1de215ea170d0acc61815dee
A Shuayto <ashuayto@As-MacBook-Pro.local>
2022-04-22 17:53:25 -0400

    refactoring to fix dictionary

--- chmod.py
+++ chmod.py
@@ -7,11 +7,38 @@
 
 
 def chmod_calculator(perm):
+
+    # Variables
     a = []
+    user = 0
+    group = 0
+    other = 0
+
+    # For Loop
     for item in perm:
         a.append((perm.get(item)))
-    return(a)
+    
+    def get_octal(a, b, part):
+        for item in a[b]:
+            if item == 'r':
+                part = part + 4
+            elif item == 'w':
+                part = part + 2
+            elif item == 'x':
+                part = part + 1
+        return part
+
+    user = (get_octal(a,0,user))
+    group = (get_octal(a,1,group))
+    other = (get_octal(a,2,other))
+
+    return (str(user) + str(group) + str(other))
+
+            
 
 
 
-print(chmod_calculator({"user": 'rwx', "group": 'r-x', "other": 'r-x'}))
+print(chmod_calculator({"user": 'rwx', "group": 'r-x', "other": 'r--'}))
+#chmod_calculator({"user": 'rwx', "group": 'r-x', "other": 'r--'})
+
+print(chmod_calculator({"group": 'rwx'})) #070
